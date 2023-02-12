d14d173a3c92a5eb034865668c97aae87cb5f851
FREDVUNI <fredvuni809@gmail.com>
2023-02-12 12:07:06 +0300

    Fix validation for the create  menu form

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -130,12 +130,12 @@ const CreateContainer = () =>{
     }
 
     return(
-        <div className="w-full min-h-screen flex items-center justify-center pt-40">
+        <div className="w-full min-h-screen flex items-center justify-center">
             <div className="w-[90%] md:w-[50%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center gap-4">
                 {
                     fields && (
-                        <motion.p initial={{ opacity:0 }} animate={{ opacity:1 }} exit={{ opacity:0 }} className={`w-full p-2 rounded-sm text-center text-lg font-semibold ${alertStatus === "danger" ? "bg-red-400 text-red-800" : "bg-emrald-400 text-emerald-800"}`}>
-                            
+                        <motion.p initial={{ opacity:0 }} animate={{ opacity:1 }} exit={{ opacity:0 }} className={`w-full p-2 rounded-sm text-center text-lg ${alertStatus === "danger" ? "bg-red-400 text-red-800" : "bg-emrald-400 text-emerald-800"}`}>
+                            {msg}
                         </motion.p>
                     )
                 }
