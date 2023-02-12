9c05a557749c13c0ee33696aaaf70be554e2d5a0
FREDVUNI <fredvuni809@gmail.com>
2023-02-12 13:02:41 +0300

    Add scroll to top on success and error

--- src/components/CreateContainer.jsx
+++ src/components/CreateContainer.jsx
@@ -81,6 +81,7 @@ const CreateContainer = () =>{
                 setFields(true)
                 setMsg('All fields are required.')
                 setAlertStatus('danger')
+                window.scroll(0,0)
 
                 setTimeout(() =>{
                     setFields(false)
@@ -100,6 +101,7 @@ const CreateContainer = () =>{
                 setIsLoading(false)
                 setFields(true)
                 setMsg('Data has been uploaded.')
+                window.scroll(0,0)
                 clearData()
                 setAlertStatus('success')
 
@@ -111,6 +113,7 @@ const CreateContainer = () =>{
         catch(error){
             console.log(error)
             setFields(true)
+            window.scroll(0,0)
             setMsg('There was an error while uploading.')
             setAlertStatus('danger')
 
@@ -130,7 +133,7 @@ const CreateContainer = () =>{
     }
 
     return(
-        <div className="w-full min-h-screen flex items-center justify-center">
+        <div className="w-full min-h-screen flex items-center justify-center pt-10">
             <div className="w-[90%] md:w-[50%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center gap-4">
                 {
                     fields && (
