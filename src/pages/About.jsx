585e2633a0b73cf80ac6523efec92d9dc4520233
FREDVUNI <fredvuni809@gmail.com>
2023-02-28 11:11:58 +0300

    Add our mission to the about section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -33,6 +33,34 @@ const About = () => {
             Deliver + fast = we are faster than you can imagine.
             We get it done, so you don’t have to worry.
         </p>
+        <h1 className="text-[3rem] lg:text-[3.5rem] font-bold tracking-wide text-headingColor">
+            Our
+            <span className="text-orange-600 text-[3rem] lg:text-[3rem] pl-2">
+                Mission
+            </span>
+        </h1>
+        <p className="text-base text-textColor md:w-[100%]">
+            Don’t worry, our team of ninjas will go out fighting to deliver your package on time.
+            Deliver + fast = we are faster than you can imagine.
+            We get it done, so you don’t have to worry.
+            Don’t worry, our team of ninjas will go out fighting to deliver your package on time.
+            Deliver + fast = we are faster than you can imagine.
+            We get it done, so you don’t have to worry.
+            Don’t worry, our team of ninjas will go out fighting to deliver your package on time.
+            Deliver + fast = we are faster than you can imagine.
+            We get it done, so you don’t have to worry.
+        </p>
+        <p className="text-base text-textColor md:w-[100%]">
+            Don’t worry, our team of ninjas will go out fighting to deliver your package on time.
+            Deliver + fast = we are faster than you can imagine.
+            We get it done, so you don’t have to worry.
+            Don’t worry, our team of ninjas will go out fighting to deliver your package on time.
+            Deliver + fast = we are faster than you can imagine.
+            We get it done, so you don’t have to worry.
+            Don’t worry, our team of ninjas will go out fighting to deliver your package on time.
+            Deliver + fast = we are faster than you can imagine.
+            We get it done, so you don’t have to worry.
+        </p>
     </div>
   )
 }
