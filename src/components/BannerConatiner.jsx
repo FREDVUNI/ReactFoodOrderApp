fb196b564a80903f418bfd7221085ff5ad319bcb
FREDVUNI <fredvuni809@gmail.com>
2023-02-28 11:02:50 +0300

    Work on the about us page and banner section

---
+++ src/components/BannerConatiner.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { heroData } from '../utils/data'
+
+const BannerConatiner = ({text}) => {
+  return (
+    <section className='w-full py-10'>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-2 w-full'>
+            <div className='flex flex-col justify-start gap-4'>
+                <p className='py-2 text-[3rem] lg:text-[3.5rem] font-bold tracking-wide text-headingColor'>{text}</p>
+            </div>
+            <div className='flex'>
+            {heroData && heroData.map((data) =>(
+                <img  key={data.id} src={data.img} alt={data.name} className="w-20 lg:w-40 -mt-10"/>
+            ))}
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default BannerConatiner
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import BannerConatiner from '../components/BannerConatiner'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+        <BannerConatiner text={'About Us'}/>
+        <h1 className="text-[3rem] lg:text-[3.5rem] font-bold tracking-wide text-headingColor">
+            Our
+            <span className="text-orange-600 text-[3rem] lg:text-[3rem] pl-2">
+                Story
+            </span>
+        </h1>
+        <p className="text-base text-textColor md:w-[100%]">
+            Don’t worry, our team of ninjas will go out fighting to deliver your package on time.
+            Deliver + fast = we are faster than you can imagine.
+            We get it done, so you don’t have to worry.
+            Don’t worry, our team of ninjas will go out fighting to deliver your package on time.
+            Deliver + fast = we are faster than you can imagine.
+            We get it done, so you don’t have to worry.
+            Don’t worry, our team of ninjas will go out fighting to deliver your package on time.
+            Deliver + fast = we are faster than you can imagine.
+            We get it done, so you don’t have to worry.
+        </p>
+        <p className="text-base text-textColor md:w-[100%]">
+            Don’t worry, our team of ninjas will go out fighting to deliver your package on time.
+            Deliver + fast = we are faster than you can imagine.
+            We get it done, so you don’t have to worry.
+            Don’t worry, our team of ninjas will go out fighting to deliver your package on time.
+            Deliver + fast = we are faster than you can imagine.
+            We get it done, so you don’t have to worry.
+            Don’t worry, our team of ninjas will go out fighting to deliver your package on time.
+            Deliver + fast = we are faster than you can imagine.
+            We get it done, so you don’t have to worry.
+        </p>
+    </div>
   )
 }
 
